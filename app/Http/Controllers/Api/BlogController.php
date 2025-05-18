b39a56b48ad2a075f48c92cc82b5fafe789645e2
abdoakram <abdoakram973@gmail.com>
2025-05-18 18:15:07 +0300

    First Commit

---
+++ app/Http/Controllers/Api/BlogController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Blog::all());
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'photo' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $path = $request->file('photo')->store('blogs', 'public');
+        $validated['photo'] = $path;
+
+        $blog = Blog::create($validated);
+
+        return response()->json($blog, 201);
+    }
+
+    public function show($id)
+    {
+        $blog = Blog::findOrFail($id);
+        return response()->json($blog);
+    }
+
+    public function update(Request $request , $id)
+    {
+        $blog = Blog::findOrFail($id);
+
+        $validated = $request->validate([
+            'title' => 'sometimes|required|string|max:255',
+            'content' => 'sometimes|required|string',
+            'photo' => 'sometimes|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('photo')) {
+            if ($blog->photo) {
+                Storage::disk('public')->delete($blog->photo);
+            }
+            $validated['photo'] = $request->file('photo')->store('blogs', 'public');
+        }
+
+        $blog->update($validated);
+
+        return response()->json($blog);
+    }
+
+    public function delete($id)
+    {
+        $blog = Blog::findOrFail($id);
+
+        if ($blog->photo) {
+            Storage::disk('public')->delete($blog->photo);
+        }
+
+        $blog->delete();
+
+        return response()->json(['message' => 'Deleted successfully']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Api\BlogController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/blogs', [BlogController::class, 'index']);
+Route::get('/blogs/show/{id}', [BlogController::class, 'show']);
+Route::post('/blogs/store', [BlogController::class, 'store']);
+Route::post('/blogs/update/{id}', [BlogController::class, 'update']);
+Route::post('/blogs/delete/{id}', [BlogController::class, 'delete']);
